a18172059c7b7babd2e80c73e06ebb3292b8a05a
joanneonis <joanne.onis@hotmail.com>
2017-06-06 23:54:02 +0200

    pull

--- js/chart.js
+++ js/chart.js
@@ -13,7 +13,11 @@ var myLineChart = new Chart(ctx, {
             label: "bloktijd",
             backgroundColor: "#D8E9FF",
             borderColor: '#4991FF',
-            borderWidth: 1.5
+            borderWidth: 1.5,
+
+            // pointBorderColor: 'rgba(0,0,0,0)',
+            // pointBackgroundColor: '#4991FF',
+            // pointRadius: 3
         },
         // {
         //     data: diepstePunt,
@@ -22,12 +26,23 @@ var myLineChart = new Chart(ctx, {
         // }
         ]
     },
-    // options: {
-    //         scales: {
-            
-    //             yAxes: [{
-    //                 stacked: true,
-    //             }]
-    //         }
-    //     }
+    options: {
+        fontColor: '#4991FF',
+        legend: {
+            display: false,
+        },
+        layout: {
+            padding: {
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+            }
+        },
+            scales: {
+                yAxes: [{
+                    
+                }]
+            }
+        }
 });
